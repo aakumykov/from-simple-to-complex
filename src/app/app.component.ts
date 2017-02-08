498c917520cc4ee1a35f82709a2bc114f5e1e4dd
Андрей Кумыков <aakumykov@yandex.ru>
2017-02-08 14:13:15 +0700

    Навигация начала вставать на ноги.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { NavController } from 'ionic-angular';
+import { MenuController } from 'ionic-angular';
 
 import { HomePage  } from '../pages/home/home';
 import { BlankPage } from '../pages/blank/blank';
@@ -13,9 +14,14 @@ import { TitlePage } from '../pages/title/title';
 
 export class MyApp {
 	@ViewChild('content') nav: NavController;
+	
 	rootPage = HomePage;
 
-	constructor(){}
+	homePage = HomePage;
+	titlePage = TitlePage;
+	blankPage = BlankPage;
+
+	constructor(public menuCtrl: MenuController){}
 
 	// ngAfterViewInit() {
 	// 	console.info('ngAfterViewInit()');
@@ -24,8 +30,8 @@ export class MyApp {
 	// }
 
 	openPage(page){
-		console.info('openPage(), page:');
-		console.info(page);
+		console.info('openPage(), page:'); console.info(page);
 		this.nav.push(page);
+		this.menuCtrl.close();
 	}
 }
