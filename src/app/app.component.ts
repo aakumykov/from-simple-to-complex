45d919966d8cc234e8f749d2ce753b9fe92c6321
Андрей Кумыков <aakumykov@yandex.ru>
2017-02-27 23:13:42 +0700

    Оставил две страницы: Начало и Районы.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,6 @@ import { NavController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
 
 import { HomePage  } from '../pages/home/home';
-import { BlankPage } from '../pages/blank/blank';
-import { TitlePage } from '../pages/title/title';
-import { GridPage } from '../pages/grid/grid';
-import { CardsPage } from '../pages/cards/cards';
-import { VideoPage } from '../pages/video/video';
-import { VideoGridPage } from '../pages/video-grid/video-grid';
-import { GifGridPage } from '../pages/gif-grid/gif-grid';
 import { RegionsPage } from '../pages/regions/regions';
 
 
@@ -21,13 +14,6 @@ export class MyApp {
 	@ViewChild('content') nav: NavController;
 
 	homePage = HomePage;
-	titlePage = TitlePage;
-	blankPage = BlankPage;
-	gridPage = GridPage;
-	cardsPage = CardsPage;
-	videoPage = VideoPage;
-	videoGridPage = VideoGridPage;
-	gifGridPage = GifGridPage;
 	regionsPage = RegionsPage;
 
 	rootPage = this.homePage;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
-import { BlankPage } from '../pages/blank/blank';
-import { TitlePage } from '../pages/title/title';
-import { GridPage } from '../pages/grid/grid';
-import { CardsPage } from '../pages/cards/cards';
-import { VideoPage } from '../pages/video/video';
-import { VideoGridPage } from '../pages/video-grid/video-grid';
-import { GifGridPage } from '../pages/gif-grid/gif-grid';
 import { RegionsPage } from '../pages/regions/regions';
 
 import { RegionsService } from '../pages/regions/regions.service';
@@ -19,13 +12,6 @@ import { RegionsService } from '../pages/regions/regions.service';
   declarations: [
     MyApp,
     HomePage,
-    BlankPage,
-    TitlePage,
-    GridPage,
-    CardsPage,
-    VideoPage,
-    VideoGridPage,
-    GifGridPage,
     RegionsPage,
   ],
   imports: [
@@ -35,13 +21,6 @@ import { RegionsService } from '../pages/regions/regions.service';
   entryComponents: [
     MyApp,
     HomePage,
-    BlankPage,
-    TitlePage,
-    GridPage,
-    CardsPage,
-    VideoPage,
-    VideoGridPage,
-    GifGridPage,
     RegionsPage,
   ],
   providers: [
