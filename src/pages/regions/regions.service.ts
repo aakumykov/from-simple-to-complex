678f17ade7f1f53029fe05bed9d3623b3d3e1ce1
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-02 16:59:13 +0700

    Работает! По образцу из учебника.

--- src/pages/regions/regions.service.ts
+++ src/pages/regions/regions.service.ts
@@ -1,20 +1,46 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable }	  			from '@angular/core';
+import { Http, Response } 			from '@angular/http';
+// import { Headers, RequestOptions }  from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { Region } from './region';
+
 
 @Injectable()
 export class RegionsService {
-	
+	private regionsUrl = 'http://localhost:3000/regions2';
+
 	constructor(private http: Http){}
 
-	regions() /*типизировать?*/ { 
-		// let regions = this.http.get('https://api.github.com/users/aakumykov/repos');
-		
-		let regions = this.http.get('http://localhost:3000/regions');
+	getRegions(): Observable<Region[]> {
+		return this.http.get(this.regionsUrl)
+						.map(this.extractData)
+						.catch(this.handleError);
+	}
 
-			console.info('--------- raw responce ---------');
-			console.info(regions);
-			console.info('------------------');
+	private extractData(res: Response) {
+		let body = res.json();
+		return body || {};
+	}
 
-		return regions;
+	private handleError(error: Response | any) {
+		// Вообще-то, нужно использовать внешнюю службу журналирования!
+		let errMsg: string;
+
+		if (error instanceof Response) {
+			const body = error.json();
+			const err = body.error || JSON.stringify(body);
+			errMsg = `$(error.status) - ${error.statusText || ''} ${err}`;
+		}
+		else {
+			errMsg = error.message ? error.message : error.toString();
+		}
+
+		console.error(errMsg);
+
+		return Observable.throw(errMsg);
 	}
 }
--- src/pages/regions/regions.ts
+++ src/pages/regions/regions.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-// import { Region } from './region';
+import { Region } from './region';
 import { RegionsService } from './regions.service';
 
 @Component({
@@ -10,46 +10,24 @@ import { RegionsService } from './regions.service';
 })
 
 export class RegionsPage {
-	public regions; //:any ?
+	// конструктор
+	constructor(private regionsService: RegionsService){}
 
-	constructor(
-		public navCtrl: NavController, 
-		public navParams: NavParams,
-		private regionsService: RegionsService,
-	) {}
+	// переменные
+	regions: Region[];
+	errorMsg: string;
 
-	ionViewDidLoad() {
-			console.log('ionViewDidLoad, RegionsPage');
-		// this.regions = this.getRegions();
+	// внешние методы
+	ngOnInit(){
+		this.getRegions();
 	}
 
-	ngOnInit(): void {
-			console.log('ngOnInit, RegionsPage');
-		this.regions = this.getRegions();
-	}
-
-	getRegions() {
-		console.info('RegionsPage.getRegions()');
-		
-		this.regionsService.regions().subscribe(
-			data => {
-				console.log('----- data -----');
-				console.log(data);
-				console.log('----------------');
-
-				this.regions = data.json();
-				//this.regions = [{"id":1,"name":"Западная Сибирь"},{"id":2,"name":"Восточная Сибирь"},{"id":3,"name":"Восточная Сибирь"},{"id":4,"name":"Дальний Восток"}];
-
-				console.info('----- this.regions -----')
-				console.info(this.regions);
-				console.info('------------------------')
-			},
-			err => {
-				console.log('----- error -----');
-				console.log(err)
-				console.log('-----------------');
-			},
-			() => console.info('getRegions выполнено')
+	// внутренние методы
+	private getRegions(){
+		this.regionsService.getRegions().subscribe(
+			regions => this.regions = regions,
+			error => this.errorMsg = <any>error//,
+			// () => this.infoMsg = 'RegionsPage.getRegions() отработал'
 		);
 	}
 }
