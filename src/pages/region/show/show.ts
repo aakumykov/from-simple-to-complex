ad9588ad6e1d14498907bde8fc25081b5db36257
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-21 10:29:20 +0700

    Редактирование из просмотра.

--- src/pages/region/show/show.ts
+++ src/pages/region/show/show.ts
@@ -23,7 +23,6 @@ export class RegionShow {
   		public navCtrl: NavController, 
   		public navParams: NavParams,
   		private regionService: RegionService,
-		private theRegion: Region,
   	) {
   		this.id = this.navParams.get('id');
   		console.info('RegionShow.constructor(), id='+this.id);
@@ -39,12 +38,6 @@ export class RegionShow {
 
 		this.regionService.getRegion(this.id).subscribe(
 			region => {
-				//this.region = region;
-				
-				this.theRegion.id = region.id;
-				this.theRegion.name = region.name;
-				this.theRegion.description = region.description;
-				
 				this.id = region.id;
 				this.name = region.name;
 				this.description = region.description;
@@ -53,18 +46,13 @@ export class RegionShow {
 		);
 	}
 
-
-	
-
-	//  ionViewCanLoad(arg)   { console.info('*ionViewCanLoad*'+arg); }
-	//  ionViewWillLoad(arg)   { console.info('*ionViewWillLoad*'+arg); }
-	// ionViewDidLoad(arg)   { console.info('*ionViewDidLoad*'+arg); }
-
-	// ionViewCanEnter(arg)  { console.info('*ionViewCanEnter*'+arg); }	
-	// ionViewWillEnter(arg) { console.info('*ionViewWillEnter*'+arg); }
-	// ionViewDidEnter(arg)  { console.info('*ionViewDidEnter*'+arg); }
-	
-	// ionViewCanLeave(arg)  { console.info('*ionViewCanLeave*'+arg); }
-	// ionViewWillLeave(arg) { console.info('*ionViewWillLeave*'+arg); }
-	// ionViewDidLeave(arg)  { console.info('*ionViewDidLeave*'+arg); }
+	editItem(item: Region){
+		console.info('RegionShow.editItem()');
+		let data = {
+			id: this.id,
+			name: this.name,
+			description: this.description,
+		}
+		this.navCtrl.push(RegionEdit,data);
+	}
 }
