0fcc7acb77e9c6d178c57229f3cb014ecedbc2c5
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-21 10:53:42 +0700

    Удаление из просмотра с подтверждением.

--- src/pages/region/show/show.ts
+++ src/pages/region/show/show.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 
 import { Region } from '../region.class';
 import { RegionService } from '../region.service';
 import { RegionEdit } from '../edit/edit';
+import { RegionList } from '../list/list';
 
 @Component({
   selector: 'region-show',
@@ -23,6 +25,7 @@ export class RegionShow {
   		public navCtrl: NavController, 
   		public navParams: NavParams,
   		private regionService: RegionService,
+  		private actionSheetCtrl: ActionSheetController,
   	) {
   		this.id = this.navParams.get('id');
   		console.info('RegionShow.constructor(), id='+this.id);
@@ -46,7 +49,7 @@ export class RegionShow {
 		);
 	}
 
-	editItem(item: Region){
+	editItem(){
 		console.info('RegionShow.editItem()');
 		let data = {
 			id: this.id,
@@ -54,5 +57,47 @@ export class RegionShow {
 			description: this.description,
 		}
 		this.navCtrl.push(RegionEdit,data);
+	}
+
+	removeItem() {
+		console.info('RegionShow.removeItem()');
+		this.presentActionSheet();
+	}
+
+	private presentActionSheet(){
+		console.info('RegionShow.presentActionSheet()');
+		
+		let actionSheet = this.actionSheetCtrl.create({
+		  title: 'Удалить «'+this.name+'»?',
+		  buttons: [
+		    {
+		      text: 'Да',
+		      role: 'destructive',
+		      handler: () => {
+		        console.info('Destructive clicked');
+		        this.removeItemReal();
+		      }
+		    },{
+		      text: 'Нет',
+		      role: 'cancel',
+		      handler: () => {
+		        console.info('Cancel clicked');
+		      }
+		    }
+		  ]
+		});
+		actionSheet.present();
+	}
+
+	private removeItemReal(){
+		console.info('RegionShow.removeItemReal(), id='+this.id);
+
+		this.regionService.removeRegion(this.id).subscribe(
+			()=>{
+				this.infoMsg = 'Удалено «'+this.name+'»';
+				this.navCtrl.push(RegionList);
+			},
+			error => this.errorMsg = error
+		);
 	}
 }
